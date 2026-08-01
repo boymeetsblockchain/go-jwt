0a18093018997f3e32e7b8f4ef0e912d122055b0
boymeetsblockchain <ajeoluwasegun18@gmail.com>
2026-08-01 07:55:15 +0100

    refactor main function to use gin for routing and add /ping endpoint

--- main.go
+++ main.go
@@ -1,14 +1,20 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/boymeetsblockchain/go-jwt/initializers"
+	"github.com/gin-gonic/gin"
 )
 
 func init() {
 	initializers.LoadEnvVariables()
 }
+
 func main() {
-	fmt.Println("Hello world 3")
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run() // listen and serve on
 }
